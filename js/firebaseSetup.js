93d8597def885a3e1133e87864bf7e41004beda2
Eli Yazdi <eli6093@gmail.com>
2017-03-14 23:01:48 -0400

    setup firebase

---
+++ js/firebaseSetup.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDlDzhHxOkpba6D1eZy5bhPRspXiv8X7vk",
+    authDomain: "hackbloomfield-001.firebaseapp.com",
+    databaseURL: "https://hackbloomfield-001.firebaseio.com",
+    storageBucket: "hackbloomfield-001.appspot.com",
+    messagingSenderId: "475566387283"
+  };
+  firebase.initializeApp(config);
