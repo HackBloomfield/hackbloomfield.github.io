b9e84314a9576a6aca76d2f96c96e5a138438c30
Eli Yazdi <eli6093@gmail.com>
2017-03-18 19:44:41 -0400

    added function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 var functions = require('firebase-functions');
 
+exports.appliedStatusUpdate = functions.database.ref('applications').onWrite(event =>{
+  var snapVal = event.data.val();
+  for(var key in snapVal){
+    functions.database.ref('appStatus/'+key).update({applied: true});
+  }
+});
 // // Start writing Firebase Functions
 // // https://firebase.google.com/functions/write-firebase-functions
 //
