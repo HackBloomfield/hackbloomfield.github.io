f9fdcfc408e2285ad10682a485140395dbf9aace
Eli Yazdi <eli6093@gmail.com>
2017-03-19 20:20:40 -0400

    added browserify

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,18 +4,20 @@ firebase.auth().onAuthStateChanged(function(user) {
     $(".loggedIn").show();
     var currentUser = firebase.auth().currentUser.uid;
     console.log(currentUser);
-    firebase.database().ref('appStatus/'+currentUser).on('value', function(snapshot){
-      var val = snapshot.val();
+    firebase.database().ref('applications/'+currentUser).once('value', function(snap){
       var appStatus = $("#appStatus");
-      if(val.applied){
-        if(val.accepted){
-          appStatus.text("Accepted");
-        }else if(val.accepted == false){
-          appStatus.text("Waitlisted");
-        }else if(val.accepted == null){
-          appStatus.text("Yet to be determined");
-        }
-      }else if(!val.applied || val.applied == null){
+      if(snap.val() != null){
+        firebase.database().ref('appStatus/'+currentUser).on('value', function(snapshot){
+          var val = snapshot.val();
+          if(val == null){
+            appStatus.text("Yet to be determined");
+          }else if(val.accepted){
+            appStatus.text("Accepted");
+          }else if(val.accepted == false){
+            appStatus.text("Waitlisted");
+          }
+        });
+      }else{
         appStatus.text("You haven't applied yet!");
       }
     });
